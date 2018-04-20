d629882bad9734ea765572bd2d472799cd06fa4b
Cynthia Rittenbach <crittenbach@gmail.com>
2018-04-20 23:01:16 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import CatList from './CatList'
+import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as actions from './actions/catActions.js'
 
-class App extends Component {   
+export class App extends Component {   
+
+  componentDidMount() {
+    if (this.props.catPics.length === 0) {
+      console.log('in component did mount')
+      this.props.actions.fetchCats()
+    }
+  }
   
   render() {
     return (
@@ -13,6 +24,7 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics} />
       </div>
     );
   }
